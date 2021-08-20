3d6ee8cd4cc4f1469a00045a2fc9ef0f67afe8b6
Probir Ghosh <probirghosh@outlook.com>
2021-08-20 18:26:30 +0600

    home component card section added

---
+++ src/components/Books/Books.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Card, Col, Button } from "react-bootstrap";
+
+const Books = (props) => {
+  const { productName, price, productImage } = props.book;
+  return (
+    <Col>
+      <Card className="shadow mb-4" style={{ borderRadius: "15px", border: 0 }}>
+        <div className="d-flex align-items-center p-2"  style={{height: "300px"}}>
+          <Card.Img
+            className="w-100 pt-3"
+              style={{ objectFit:"contain",maxHeight:"300px" }}
+            
+            src={productImage}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title as={"h3"}>{productName}</Card.Title>
+        </Card.Body>
+        <Card.Body className="d-flex justify-content-between align-items-center" >
+          <Card.Title
+          as={"h2"}
+          style={{ fontWeight:"700", color:"#ff4b2b" }}>
+              ${price}
+          </Card.Title>
+          <Button className="button__style shadow-none" >Buy Now</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Books;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,49 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-
+import axios from 'axios';
+import Books from "../Books/Books";
 const Home = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(watch("example")); 
+    const { register, handleSubmit, formState: { errors } } = useForm();
+   const [books, setBooks] = useState([]);
+
+
+   useEffect(() => {
+       axios.get('https://electro-server.herokuapp.com/products')
+       .then(res=>{
+           setBooks(res.data);
+       })
+
+       .catch(err=>{
+           console.log(err);
+       })
+      
+   }, [])
+
+
+    const onSubmit = data =>{
+        axios.get(`https://electro-server.herokuapp.com/search?keyword=${data.keyword}`)
+        .then(res=>{
+            setBooks(res.data);
+        })
+
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+    
 
   return (
     <Container>
       <form className="search__container" onSubmit={handleSubmit(onSubmit)}>
-        <input className="search__input"  placeholder="Search" defaultValue="" {...register("search-input")} />
+        <input className="search__input"  placeholder="Search" name="keyword" type="text" defaultValue="" {...register("search-input")} />
         <button className="search__button">Search</button>
       </form>
+      <Row xs={1} sm={1} md={2} lg={3}>
+          {
+             books.map(bookIdentity=> <Books book={bookIdentity} /> )
+          }
+      </Row>
     </Container>
   );
 };
